15656097d1fd4df0b2a30da8b151e4b23f2c2acc
Tmarra122333 <Tmarra122333@gmail.com>
2022-02-09 12:13:15 -0500

    complete

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -63,6 +63,6 @@ SELECT * FROM city WHERE id = 211;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT * FROM city WHERE population = 91084;
 
-
--- She's in ______________________!
+-- She's in SANTA MONICA!
