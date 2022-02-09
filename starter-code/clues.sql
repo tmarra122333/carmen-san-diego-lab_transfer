c51d5c624a2d3d64fc868514832787d51af23301
Tmarra122333 <Tmarra122333@gmail.com>
2022-02-09 12:12:06 -0500

    Closer

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -39,12 +39,15 @@ SELECT * FROM city WHERE name LIKE 'Serra%';
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+SELECT capital FROM country WHERE code = 'BRA';
 
+-- 211 is the returned code. (brasilia, distrito federal)
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
 
+SELECT * FROM city WHERE id = 211;
 
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
